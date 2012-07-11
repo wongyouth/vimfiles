cf16272561b156a40027134ed1cc060b1f48574b
Yongzhi Wang <wongyouth@gmail.com>
2012-07-11 15:24:08 +0800

    backup existed config files

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# backup existed config files
+for i in .vim vimfiles .vimrc .gvimrc; do
+  if [ -e ~/$i -o -h ~/$i ]; then
+    mv -f ~/$i ~/$i.old
+  fi
+done
+
 git clone git://github.com/wongyouth/vimfiles ~/vimfiles
 ln -s ~/vimfiles ~/.vim
 echo "source ~/.vim/vimrc" > ~/.vimrc
