946b42a1f137d88d95ae3a1ce70de52026bdcd73
Yongzhi Wang <wongyouth@gmail.com>
2012-07-11 15:36:19 +0800

    rm old backup before new backup

--- install.sh
+++ install.sh
@@ -2,7 +2,13 @@
 
 # backup existed config files
 for i in .vim vimfiles .vimrc .gvimrc; do
+  # file or symbol link
   if [ -e ~/$i -o -h ~/$i ]; then
+    # rm old backup
+    if [ -e ~/$i.old ]; then
+      rm -rf ~/$i.old
+    fi
+    # backup file or folder
     mv -f ~/$i ~/$i.old
   fi
 done
